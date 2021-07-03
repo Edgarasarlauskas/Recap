5ca58df3eeb44b7207832d0d7db3fbaf3b303575
EdgarasArlauskas <edgaras.arlauskass@gmail.com>
2021-07-03 11:41:03 +0300

    added Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        byte bitas = -100;
+        short shortas = 1;
+        long longas = 1333;
+        float floatas = 1.22f;
+        double dvigubas = 1.22;
+        int age = 27;
+
+        boolean isMarried = true;
+        char creditClass = 'A';
+
+        int[] gatvesNR = new int[4];
+
+        String labas = "labas";
+
     }
 }
